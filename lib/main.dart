47f0eec4945efdd510fbd9d41d60a807148d974f
widya0 <widyadhana077@gmail.com>
2024-01-24 16:20:46 +0700

    memperbaiki halaman utama

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_scanner/login.dart';
 import 'package:flutter_scanner/menu_utama.dart';
-<<<<<<< HEAD
 import 'package:flutter_scanner/riwayat_proses.dart';
-=======
->>>>>>> 22370ea6fc2ce9a066b6b4e83eae8b519ce004ef
 
 void main() {
   runApp(MyApp());
@@ -19,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: loginpage(),
+      home: MenuUtama(),
     );
   }
 }
--- lib/menu_utama.dart
+++ lib/menu_utama.dart
@@ -7,41 +7,110 @@ class MenuUtama extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Row(
+          child: Column(
+        children: [
+          Row(children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-<<<<<<< HEAD
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-=======
-                Expanded(
-                  child:
-                  Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
->>>>>>> 22370ea6fc2ce9a066b6b4e83eae8b519ce004ef
-                  children: [
-                    Text("REKA CHAIN",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    Text(
-                      "Selamat Datang...",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                  ),
-                ),
-                Image.asset(
-                  'assets/images/bolder32.png',
-                  width: 200,
-                  alignment: Alignment.topRight,
-                ),
+                Text("REKA CHAIN",
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                Text(
+                  "Selamat Datang...",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                )
               ],
-            )
-          ],
-        ),
-      ),
+            ),
+          ]),
+          Image.asset(
+            'assets/images/bolder32.png',
+            width: 200,
+            alignment: Alignment.topRight,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.qr_code_scanner_rounded,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Scan Material",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(width: 40),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.qr_code_scanner_rounded,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Scan Produk",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.edit_document,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Download Document",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(width: 40),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.check_box_outlined,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Cek Status Produk",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
