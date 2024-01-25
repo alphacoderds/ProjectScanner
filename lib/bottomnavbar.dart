5270460f151533b6bf5377360640fbe4d9d772d8
widya0 <widyadhana077@gmail.com>
2024-01-25 12:57:13 +0700

    menambahkan bottomnavbar

---
+++ lib/bottomnavbar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_scanner/menu_utama.dart';
+
+/// Flutter code sample for [BottomNavigationBar].
+
+void main() => runApp(const BottomNavigationBarExampleApp());
+
+class BottomNavigationBarExampleApp extends StatelessWidget {
+  const BottomNavigationBarExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BottomNavigationBarExample(),
+    );
+  }
+}
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    MenuUtama(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/menu_utama.dart
+++ lib/menu_utama.dart
@@ -1,116 +1,154 @@
 import 'package:flutter/material.dart';
 
-class MenuUtama extends StatelessWidget {
-  const MenuUtama({super.key});
+class MenuUtama extends StatefulWidget {
+  const MenuUtama({Key? key}) : super(key: key);
+
+  @override
+  _MenuUtamaState createState() => _MenuUtamaState();
+}
+
+class _MenuUtamaState extends State<MenuUtama> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
           child: Column(
-        children: [
-          Row(children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("REKA CHAIN",
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                Text(
-                  "Selamat Datang...",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                )
-              ],
+            children: [
+              Row(children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("REKA CHAIN",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    Text(
+                      "Selamat Datang...",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ]),
+              Image.asset(
+                'assets/images/bolder32.png',
+                width: 200,
+                alignment: Alignment.topRight,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        color: Color.fromRGBO(85, 100, 136, 1),
+                        child: Icon(
+                          Icons.qr_code_scanner_rounded,
+                          color: Color.fromRGBO(43, 56, 86, 1),
+                          size: 70,
+                        ),
+                      ),
+                      Text(
+                        "Scan Material",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(width: 60),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        color: Color.fromRGBO(85, 100, 136, 1),
+                        child: Icon(
+                          Icons.qr_code_scanner_rounded,
+                          color: Color.fromRGBO(43, 56, 86, 1),
+                          size: 70,
+                        ),
+                      ),
+                      Text(
+                        "Scan Produk",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        color: Color.fromRGBO(85, 100, 136, 1),
+                        child: Icon(
+                          Icons.description_outlined,
+                          color: Color.fromRGBO(43, 56, 86, 1),
+                          size: 70,
+                        ),
+                      ),
+                      Text(
+                        "Download Document",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        color: Color.fromRGBO(85, 100, 136, 1),
+                        child: Icon(
+                          Icons.assignment_turned_in_outlined,
+                          color: Color.fromRGBO(43, 56, 86, 1),
+                          size: 70,
+                        ),
+                      ),
+                      Text(
+                        "Cek Status Produk",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-          ]),
-          Image.asset(
-            'assets/images/bolder32.png',
-            width: 200,
-            alignment: Alignment.topRight,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    color: Color.fromRGBO(85, 100, 136, 1),
-                    child: Icon(
-                      Icons.qr_code_scanner_rounded,
-                      color: Color.fromRGBO(43, 56, 86, 1),
-                      size: 70,
-                    ),
-                  ),
-                  Text(
-                    "Scan Material",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-              SizedBox(width: 40),
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    color: Color.fromRGBO(85, 100, 136, 1),
-                    child: Icon(
-                      Icons.qr_code_scanner_rounded,
-                      color: Color.fromRGBO(43, 56, 86, 1),
-                      size: 70,
-                    ),
-                  ),
-                  Text(
-                    "Scan Produk",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    color: Color.fromRGBO(85, 100, 136, 1),
-                    child: Icon(
-                      Icons.edit_document,
-                      color: Color.fromRGBO(43, 56, 86, 1),
-                      size: 70,
-                    ),
-                  ),
-                  Text(
-                    "Download Document",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-              SizedBox(width: 40),
-              Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    color: Color.fromRGBO(85, 100, 136, 1),
-                    child: Icon(
-                      Icons.check_box_outlined,
-                      color: Color.fromRGBO(43, 56, 86, 1),
-                      size: 70,
-                    ),
-                  ),
-                  Text(
-                    "Cek Status Produk",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                  )
-                ],
-              ),
-            ],
-          )
-        ],
-      )),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color.fromRGBO(43, 56, 86, 1),
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+      ),
     );
   }
 }
