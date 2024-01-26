fded875c2b4cfa29140c855d0b1678132351f224
widya0 <widyadhana077@gmail.com>
2024-01-26 13:56:09 +0700

    bottomnavbar

--- lib/menu_utama.dart
+++ lib/menu_utama.dart
@@ -8,203 +8,171 @@ class MenuUtama extends StatefulWidget {
 }
 
 class _MenuUtamaState extends State<MenuUtama> {
-  int _selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 0, left: 315),
-              child: Image.asset(
-                'assets/images/bolder32.png',
-                width: 150,
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 0, left: 315),
+            child: Image.asset(
+              'assets/images/bolder32.png',
+              width: 150,
+            ),
+          ),
+          SizedBox(height: 20),
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 0, left: 40),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "REKA CHAIN",
+                      style: TextStyle(
+                          fontFamily: 'Donegal One',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Selamat Datang...",
+                      style:
+                          TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Stack(
-              alignment: Alignment.topRight,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 0, left: 40),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "REKA CHAIN",
-                        style: TextStyle(
-                            fontFamily: 'Donegal One', fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "Selamat Datang...",
-                        style: TextStyle(
-                            fontSize: 23, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+            ],
+          ),
 
-            SizedBox(height: 40), // Add some space between the text and image
-            Stack(
-              alignment: Alignment.topRight,
-            ),
-            SizedBox(
-                height: 20), // Add some space between the image and buttons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      color: Color.fromRGBO(85, 100, 136, 1),
-                      child: Icon(
-                        Icons.qr_code_scanner_rounded,
-                        color: Color.fromRGBO(43, 56, 86, 1),
-                        size: 70,
-                      ),
-                    ),
-                    Text(
-                      "Scan Material",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                SizedBox(width: 30),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      color: Color.fromRGBO(85, 100, 136, 1),
-                      child: Icon(
-                        Icons.move_to_inbox_rounded,
-                        color: Color.fromRGBO(43, 56, 86, 1),
-                        size: 70,
-                      ),
-                    ),
-                    Text(
-                      "Scan Produk",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                SizedBox(width: 30),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      color: Color.fromRGBO(85, 100, 136, 1),
-                      child: Icon(
-                        Icons.qr_code_scanner_rounded,
-                        color: Color.fromRGBO(43, 56, 86, 1),
-                        size: 70,
-                      ),
-                    ),
-                    Text(
-                      "Scan Produk",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 20), // Add some space between the button rows
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      color: Color.fromRGBO(85, 100, 136, 1),
-                      child: Icon(
-                        Icons.description_outlined,
-                        color: Color.fromRGBO(43, 56, 86, 1),
-                        size: 70,
-                      ),
-                    ),
-                    Text(
-                      "Download \nDocument",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                SizedBox(width: 30),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      color: Color.fromRGBO(85, 100, 136, 1),
-                      child: Icon(
-                        Icons.headset_mic,
-                        color: Color.fromRGBO(43, 56, 86, 1),
-                        size: 70,
-                      ),
-                    ),
-                    Text(
-                      "After Seles",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                SizedBox(width: 30),
-                Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      color: Color.fromRGBO(85, 100, 136, 1),
-                      child: Icon(
-                        Icons.assignment_turned_in_outlined,
-                        color: Color.fromRGBO(43, 56, 86, 1),
-                        size: 70,
-                      ),
-                    ),
-                    Text(
-                      "Cek Status \n   Produk",
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+          SizedBox(height: 40), // Add some space between the text and image
+          Stack(
+            alignment: Alignment.topRight,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+          SizedBox(height: 20), // Add some space between the image and buttons
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.qr_code_scanner_rounded,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Scan Material",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(width: 30),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.move_to_inbox_rounded,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Scan Produk",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(width: 30),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.qr_code_scanner_rounded,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Scan Produk",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+          SizedBox(height: 20), // Add some space between the button rows
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.description_outlined,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Download \nDocument",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(width: 30),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.headset_mic,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "After Seles",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              SizedBox(width: 30),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    color: Color.fromRGBO(85, 100, 136, 1),
+                    child: Icon(
+                      Icons.assignment_turned_in_outlined,
+                      color: Color.fromRGBO(43, 56, 86, 1),
+                      size: 70,
+                    ),
+                  ),
+                  Text(
+                    "Cek Status \n   Produk",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromRGBO(43, 56, 86, 1),
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
       ),
-    );
+    ));
   }
 }
