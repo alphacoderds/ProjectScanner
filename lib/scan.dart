9b3c85694ce51b5ccadf57dfaa4065cb8ca0bc28
nabilaeka123 <nabila2004eka@gmail.com>
2024-01-30 10:17:38 +0700

    Perbaikan scan

--- lib/scan.dart
+++ lib/scan.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,38 +10,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: scan(),
+      home: MyHomePage(),
     );
   }
 }
 
-class scan extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'cancel',
+        true,
+        ScanMode.BARCODE,
+      );
+      debugPrint(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version';
+    }
+    if (!mounted) return;
+    setState(() {
+      // Lakukan perubahan state jika diperlukan setelah pemindaian barcode
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        title: Row(
           children: [
             Icon(
-              Icons.arrow_back, // Icon panah ke kiri
+              Icons.arrow_back,
               color: Color.fromARGB(255, 10, 10, 10),
             ),
-            SizedBox(width: 8), // Jarak antara icon dan teks
+            SizedBox(width: 8),
             Text('Reka Chain'),
           ],
         ),
       ),
       body: Align(
-        alignment: Alignment(0, -0.5), // Mengatur posisi tombol ke atas
+        alignment: Alignment(0, -0.5),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ElevatedButton(
               onPressed: () {
-                // Aksi yang ingin Anda lakukan saat tombol "Scan" ditekan
-                print('Melakukan scanning...');
+                scanBarcodeNormal(); // Panggil fungsi untuk melakukan pemindaian barcode
               },
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.all(16),
@@ -55,42 +79,10 @@ class scan extends StatelessWidget {
                     Icons.center_focus_weak,
                     size: 180,
                   ),
-                  SizedBox(height: 20), // Jarak antara ikon dan teks
+                  SizedBox(height: 20),
                   Text('Scan'),
                 ],
               ),
-            ),
-            SizedBox(
-                height: 20), // Jarak antara tombol "Scan" dan tombol "Process"
-            ElevatedButton(
-              onPressed: () {
-                // Aksi yang ingin Anda lakukan saat tombol "Process" ditekan
-                print('Processing...');
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                primary: Colors.white, // Warna putih untuk tombol "Process"
-              ),
-              child: Text('Process'),
-            ),
-            SizedBox(
-                height:
-                    20), // Jarak antara tombol "Process" dan pemenuhan kekurangan
-            ElevatedButton(
-              onPressed: () {
-                // Aksi yang ingin Anda lakukan saat tombol pemenuhan kekurangan ditekan
-                print('Pemenuhan Kekurangan...');
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.all(16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: Text('Pemenuhan Kekurangan'),
             ),
           ],
         ),
