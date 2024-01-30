28abf084ade495764879cfc9f58972b35a7a2311
Mimingming10 <natasyain10@gmail.com>
2024-01-30 13:53:17 +0700

    Scan Produk

---
+++ lib/scan_material.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'cancel',
+        true,
+        ScanMode.BARCODE,
+      );
+      debugPrint(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version';
+    }
+    if (!mounted) return;
+    setState(() {
+      // Lakukan perubahan state jika diperlukan setelah pemindaian barcode
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(
+              Icons.arrow_back,
+              color: Color.fromARGB(255, 10, 10, 10),
+            ),
+            SizedBox(width: 8),
+            Text('Reka Chain'),
+          ],
+        ),
+      ),
+      body: Align(
+        alignment: Alignment(0, -0.5),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                scanBarcodeNormal(); // Panggil fungsi untuk melakukan pemindaian barcode
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.all(16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.center_focus_weak,
+                    size: 180,
+                  ),
+                  SizedBox(height: 20),
+                  Text('Scan'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/scan_pruduk.dart
+++ lib/scan_pruduk.dart
@@ -3,28 +3,24 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scanner(),
+    return MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
-class Scanner extends StatefulWidget {
-  const Scanner({super.key});
-
+class MyHomePage extends StatefulWidget {
   @override
-  _ScannerState createState() => _ScannerState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _ScannerState extends State<Scanner> {
+class _MyHomePageState extends State<MyHomePage> {
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
     try {
@@ -35,6 +31,14 @@ class _ScannerState extends State<Scanner> {
         ScanMode.BARCODE,
       );
       debugPrint(barcodeScanRes);
+
+      // Tampilkan pesan setelah selesai melakukan scan
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Hasil scan: $barcodeScanRes'),
+          duration: Duration(seconds: 3),
+        ),
+      );
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version';
     }
@@ -46,7 +50,7 @@ class _ScannerState extends State<Scanner> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Row(
+        title: Row(
           children: [
             Icon(
               Icons.arrow_back,
@@ -58,22 +62,22 @@ class _ScannerState extends State<Scanner> {
         ),
       ),
       body: Align(
-        alignment: const Alignment(0, -0.5),
+        alignment: Alignment(0, -0.5),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ElevatedButton(
               onPressed: () {
-                scanBarcodeNormal(); // Panggil fungsi untuk melakukan pemindaian barcode
+                scanBarcodeNormal();
               },
               style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(16),
+                padding: EdgeInsets.all(16),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-              child: const Column(
+              child: Column(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
