1e9f398c9432603bec2f22d3fd16cde5a1f582fd
Mimingming10 <natasyain10@gmail.com>
2024-02-06 15:42:31 +0700

    perbaikan pop up

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_scanner/bottomnavbar.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
