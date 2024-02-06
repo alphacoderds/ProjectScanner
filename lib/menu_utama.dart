04669091e8f207875bbbbb01b8af0afe199354e5
Mimingming10 <natasyain10@gmail.com>
2024-02-06 09:01:37 +0700

    responsif

--- lib/menu_utama.dart
+++ lib/menu_utama.dart
@@ -16,260 +16,192 @@ class MenuUtama extends StatefulWidget {
 class _MenuUtamaState extends State<MenuUtama> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 0, left: 315),
-              child: Image.asset(
-                'assets/images/bolder32.png',
-                width: 150,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Stack(
-              alignment: Alignment.topRight,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: Stack(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 0, left: 40),
+                Center(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text(
-                        "REKA CHAIN",
-                        style: TextStyle(
-                          fontFamily: 'Donegal One',
-                          fontWeight: FontWeight.bold,
-                        ),
+                      SizedBox(height: screenHeight * 0.05),
+                      Stack(
+                        alignment: Alignment.topRight,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: screenHeight * 0.01,
+                              left: screenWidth * 0.1,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "REKA CHAIN",
+                                  style: TextStyle(
+                                    fontFamily: 'Donegal One',
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  "Selamat Datang...",
+                                  style: TextStyle(
+                                    fontSize: screenWidth * 0.05,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Image.asset(
+                              'assets/images/bolder32.png',
+                              width: screenWidth * 0.2,
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        "Selamat Datang...",
-                        style: TextStyle(
-                          fontSize: 23,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      SizedBox(height: screenHeight * 0.04),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          buildMenuItem(
+                            screenWidth,
+                            Icons.qr_code_scanner_rounded,
+                            "Scan Material",
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const ScannerMaterial(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(width: screenWidth * 0.05),
+                          buildMenuItem(
+                            screenWidth,
+                            Icons.move_to_inbox_rounded,
+                            "Scan Produk",
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const ScannerProduk(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(width: screenWidth * 0.05),
+                          buildMenuItem(
+                            screenWidth,
+                            Icons.move_to_inbox_rounded,
+                            "Riwayat Scan",
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const MenuRiwayatScan(),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: screenHeight * 0.03),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          buildMenuItem(
+                            screenWidth,
+                            Icons.description_outlined,
+                            "Unduh Berkas",
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const unduh(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(width: screenWidth * 0.05),
+                          buildMenuItem(
+                            screenWidth,
+                            Icons.headset_mic,
+                            "After Sales",
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const ScannerAfterSales(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(width: screenWidth * 0.05),
+                          buildMenuItem(
+                            screenWidth,
+                            Icons.move_to_inbox_rounded,
+                            "Tahap Selesai",
+                            () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const ScannerTahapSelesai(),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 40),
-            const Stack(
-              alignment: Alignment.topRight,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildMenuItem(
+      double screenWidth, IconData icon, String label, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(screenWidth * 0.04),
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(85, 100, 136, 1),
+              borderRadius: BorderRadius.circular(screenWidth * 0.1),
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ScannerMaterial()),
-                    );
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(85, 100, 136, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Icon(
-                          Icons.qr_code_scanner_rounded,
-                          color: Color.fromRGBO(43, 56, 86, 1),
-                          size: 70,
-                        ),
-                      ),
-                      const Text(
-                        "Scan Material",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 30),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ScannerProduk()),
-                    );
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(85, 100, 136, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Icon(
-                          Icons.move_to_inbox_rounded,
-                          color: Color.fromRGBO(43, 56, 86, 1),
-                          size: 70,
-                        ),
-                      ),
-                      const Text(
-                        "Scan Produk",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 30),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const MenuRiwayatScan()),
-                    );
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(85, 100, 136, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Icon(
-                          Icons.move_to_inbox_rounded,
-                          color: Color.fromRGBO(43, 56, 86, 1),
-                          size: 70,
-                        ),
-                      ),
-                      const Text(
-                        "Riwayat Scan",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
+            child: Icon(
+              icon,
+              color: Color.fromRGBO(43, 56, 86, 1),
+              size: screenWidth * 0.2,
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const unduh()),
-                    );
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(85, 100, 136, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Icon(
-                          Icons.description_outlined,
-                          color: Color.fromRGBO(43, 56, 86, 1),
-                          size: 70,
-                        ),
-                      ),
-                      const Text(
-                        "Unduh Berkas",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 30),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ScannerAfterSales()),
-                    );
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(85, 100, 136, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Icon(
-                          Icons.headset_mic,
-                          color: Color.fromRGBO(43, 56, 86, 1),
-                          size: 70,
-                        ),
-                      ),
-                      const Text(
-                        "After Seles",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                const SizedBox(width: 30),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ScannerTahapSelesai()),
-                    );
-                  },
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(85, 100, 136, 1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: const Icon(
-                          Icons.move_to_inbox_rounded,
-                          color: Color.fromRGBO(43, 56, 86, 1),
-                          size: 70,
-                        ),
-                      ),
-                      const Text(
-                        "Tahap Selesai",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
+          ),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: screenWidth * 0.04,
+              fontWeight: FontWeight.bold,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
