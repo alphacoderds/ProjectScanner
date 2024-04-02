7ecf78cc149751ed5eb3ef7f9ee0598e0e8fa3af
widya0 <widyadhana077@gmail.com>
2024-04-02 11:13:19 +0700

    scan material

--- lib/API/login.php
+++ lib/API/login.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-$conn=mysqli_connect('localhost','root','','db_contact');
+$conn=mysqli_connect('localhost','root','','db_rekachain');
 
 $nip=$_POST['nip'];
 $password=$_POST['password'];
