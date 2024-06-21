90d14f9861d35b5f0ec12b4c318c70c61c1ece16
cacalawak <risayusnita02@gmail.com>
2024-06-21 15:40:41 +0700

    refresh

--- lib/unduhberkas/get_unduh.php
+++ lib/unduhberkas/get_unduh.php
@@ -4,7 +4,7 @@ $conn=mysqli_connect('localhost','root','','db_rekachain');
 
 $id_lot = $_GET['id_lot'];
 
-$sql = $conn->query("SELECT tbl_lot.*, tbl_file.file 
+$sql = $conn->query("SELECT tbl_lot.*, tbl_file.file, tbl_file.tanggal 
 FROM tbl_lot 
 JOIN tbl_file ON tbl_lot.noProduk = tbl_file.noProduk 
 WHERE tbl_lot.id_lot = '$id_lot'");
